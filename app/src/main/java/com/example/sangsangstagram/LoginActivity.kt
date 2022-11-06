488259052b1a5fa00ba9b7d57aa795774f49c764
oh in seong <superohinsung@naver.com>
2022-11-06 15:47:37 +0900

    로그인 화면 UI 구현 및 기능 구현

--- app/src/main/java/com/example/sangsangstagram/LoginActivity.kt
+++ app/src/main/java/com/example/sangsangstagram/LoginActivity.kt
@@ -1,9 +1,13 @@
 package com.example.sangsangstagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.view.WindowCompat
 import com.example.sangsangstagram.databinding.ActivityLoginBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +16,24 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         WindowCompat.setDecorFitsSystemWindows(window, true)
+        binding.loginButton.setOnClickListener {
+            val userEmail = binding.editId.text.toString()
+            val password = binding.editPwd.text.toString()
+            doLogin(userEmail, password)
+        }
+    }
+
+    private fun doLogin(userEmail: String, password: String) {
+        Firebase.auth.signInWithEmailAndPassword(userEmail, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    startActivity(
+                        Intent(this, MainActivity::class.java)
+                    )
+                    finish()
+                } else {
+                    Toast.makeText(this, "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
