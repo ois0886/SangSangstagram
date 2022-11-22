92e6c0c792df01c6861c4f401cd5e9bb532c0938
jieun <92997924+JinjinBread@users.noreply.github.com>
2022-11-22 15:37:47 +0900

    Create PostViewHolder.kt

---
+++ app/src/main/java/com/example/sangsangstagram/view/home/post/PostViewHolder.kt
@@ -0,0 +1,75 @@
+package com.example.sangsangstagram.view.home.post
+
+import android.annotation.SuppressLint
+import android.graphics.Typeface
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
+import android.widget.ToggleButton
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.sangsangstagram.R
+import com.example.sangsangstagram.databinding.ItemPostBinding
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
+class PostViewHolder(
+    private val binding: ItemPostBinding,
+    private val onClickUser: (PostItemUiState) -> Unit,
+    private val onClickLikeButton: (PostItemUiState) -> Unit,
+    private val onClickMoreButton: (PostItemUiState) -> Unit
+) : RecyclerView.ViewHolder(binding.root) {
+
+    private val storageReference = Firebase.storage.reference
+
+    fun bind(uiState: PostItemUiState) = with(binding) {
+        val glide = Glide.with(root)
+
+        glide.load(uiState.writerProfileImageUrl?.let { storageReference.child(it) })
+            .fallback(R.drawable.ic_baseline_person_pin_24)
+            .into(profileImage)
+
+        userName.text = uiState.writerName
+        userName.setOnClickListener {
+            onClickUser(uiState)
+        }
+        profileImage.setOnClickListener {
+            onClickUser(uiState)
+        }
+
+        moreInfoButton.isVisible = uiState.isMine
+        moreInfoButton.setOnClickListener {
+            onClickMoreButton(uiState)
+        }
+
+        glide.load(storageReference.child(uiState.imageUrl))
+            .into(postImage)
+
+        likeToggleButton.isChecked = uiState.meLiked
+        likeToggleButton.setOnClickListener {
+            val isChecked = (it as ToggleButton).isChecked
+            val likeCountText = uiState.likeCount +
+                    (if (uiState.meLiked) -1 else 0) +
+                    (if (isChecked) 1 else 0)
+
+            likeCount.text = root.context.getString(R.string.likeCount, likeCountText)
+            onClickLikeButton(uiState)
+        }
+
+        likeCount.text = root.context.getString(R.string.likeCount, uiState.likeCount)
+
+        @SuppressLint("SetTextI18n")
+        val spannable = SpannableString("${uiState.writerName} ${uiState.content}")
+        spannable.setSpan(
+            StyleSpan(Typeface.BOLD),
+            0,
+            uiState.writerName.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        content.text = spannable
+        content.isVisible = uiState.content.isNotEmpty()
+
+        // timeAgo.text = uiState.timeAgo
+    }
+}
