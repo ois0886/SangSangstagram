8e35bbfebb5b1d39053ce3be70a9562ebb145b8b
jieun <92997924+JinjinBread@users.noreply.github.com>
2022-11-22 15:07:26 +0900

    Post create 기능 구현

--- app/src/main/java/com/example/sangsangstagram/view/home/post/postcreate/PostCreateUiState.kt
+++ app/src/main/java/com/example/sangsangstagram/view/home/post/postcreate/PostCreateUiState.kt
@@ -1,4 +1,13 @@
 package com.example.sangsangstagram.view.home.post.postcreate
 
-class PostCreateUiState {
-}
+import android.net.Uri
+import androidx.annotation.StringRes
+
+data class PostCreateUiState(
+    val selectedImage: Uri? = null,
+    @StringRes
+    val userMessage: Int? = null,
+    val isCreating: Boolean = true,
+    val isLoading: Boolean = false,
+    val successToUpload: Boolean = false
+)
--- app/src/main/java/com/example/sangsangstagram/view/home/post/postcreate/PostCreateViewModel.kt
+++ app/src/main/java/com/example/sangsangstagram/view/home/post/postcreate/PostCreateViewModel.kt
@@ -1,6 +1,66 @@
 package com.example.sangsangstagram.view.home.post.postcreate
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sangsangstagram.R
+import com.example.sangsangstagram.data.PostRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class PostCreateViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(PostCreateUiState())
+    val uiState = _uiState.asStateFlow()
+
+    fun selectImage(uri: Uri) {
+        _uiState.update { it.copy(selectedImage = uri) }
+    }
+
+    fun changeToEditMode() {
+        _uiState.update { it.copy(isCreating = false) }
+    }
+
+    fun uploadContent(content: String) {
+        _uiState.update { it.copy(isLoading = true) }
+        viewModelScope.launch {
+            val result = PostRepository.uploadPost(content, uiState.value.selectedImage!!)
+            if (result.isSuccess) {
+                _uiState.update { it.copy(successToUpload = true, isLoading = false) }
+            } else {
+                _uiState.update {
+                    it.copy(
+                        userMessage = R.string.failed,
+                        isLoading = false
+                    )
+                }
+            }
+        }
+    }
+
+    fun editContent(uuid: String, content: String) {
+        _uiState.update { it.copy(isLoading = true) }
+        viewModelScope.launch {
+            val result: Result<Unit> = if (_uiState.value.selectedImage != null) {
+                PostRepository.editPost(uuid, content, uiState.value.selectedImage!!)
+            } else {
+                PostRepository.editPostOnlyContent(uuid, content)
+            }
+            if (result.isSuccess) {
+                _uiState.update { it.copy(successToUpload = true, isLoading = false) }
+            } else {
+                _uiState.update {
+                    it.copy(
+                        userMessage = R.string.failed,
+                        isLoading = false
+                    )
+                }
+            }
+        }
+    }
+
+    fun userMessageShown() {
+        _uiState.update { it.copy(userMessage = null) }
+    }
 }
