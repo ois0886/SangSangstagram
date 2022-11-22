71286de800e68a37fbce4e02abaf16109dd0f4ec
jieun <92997924+JinjinBread@users.noreply.github.com>
2022-11-22 15:37:13 +0900

    create PostUiState

---
+++ app/src/main/java/com/example/sangsangstagram/view/home/post/PostUiState.kt
@@ -0,0 +1,40 @@
+package com.example.sangsangstagram.view.home.post
+
+import androidx.annotation.StringRes
+import androidx.paging.PagingData
+import com.example.sangsangstagram.domain.model.Post
+
+
+data class PostListUiState(
+    val pagingData: PagingData<PostItemUiState> = PagingData.empty(),
+    val selectedPostItem: PostItemUiState? = null,
+    val currentUserUuid: String,
+    @StringRes
+    val userMessage: Int? = null
+)
+
+data class PostItemUiState(
+    val uuid: String,
+    val writerUuid: String,
+    val writerName: String,
+    val writerProfileImageUrl: String?,
+    val content: String,
+    val imageUrl: String,
+    val likeCount: Int,
+    val meLiked: Boolean,
+    val isMine: Boolean
+    //val timeAgo: String
+)
+
+fun Post.toUiState() = PostItemUiState(
+    uuid = uuid,
+    writerUuid = writerUuid,
+    writerName = writerName,
+    writerProfileImageUrl = writerProfileImageUrl,
+    content = content,
+    imageUrl = imageUrl,
+    likeCount = likeCount,
+    meLiked = meLiked,
+    isMine = isMine
+//    timeAgo = timeAgo
+)
